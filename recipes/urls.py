761aeb823e7a6f24b8f1eabad74adaf6e1a0a480
Ibtissam <ibtissam.bahaj@hotmail.com>
2026-02-26 22:00:00 +0300

    feat: register category endpoints in URLs

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,17 @@
 #The "GPS" that tells Django which View to run based on the URL.
 
 from django.urls import path
-from .views import RecipeList, RecipeDetail
+from .views import RecipeList, RecipeDetail, CategoryList, CategoryDetail
 
 urlpatterns = [
+    # Recipe Endpoints
     # Path: /api/recipes/
     path('recipes/', RecipeList.as_view(), name='recipe-list'),
     
     # Path: /api/recipes/1/ (The <int:pk> captures the ID of the recipe)
     path('recipes/<int:pk>/', RecipeDetail.as_view(), name='recipe-detail'),
+
+    # Category Endpoints
+    path('categories/', CategoryList.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryDetail.as_view(), name='category-detail'),
 ]
