2b0441f5b5d9b6b7805c5d29de59ae6e36f3e984
Ibtissam <ibtissam.bahaj@hotmail.com>
2026-03-09 04:51:39 +0300

    feat: add user serializer and user list view

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+# Use this ONLY for the register endpoint (POST)
 class RegisterSerializer(serializers.ModelSerializer):
     # We want the password to be 'write_only' so it never shows 
     # up in the API response after the user is created.
@@ -22,3 +23,9 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data.get('email', '')
         )
         return user
+    
+# Use this for seeing all users (GET)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 class RegisterView(generics.CreateAPIView):
     # This view only allows POST requests to create a new user
@@ -13,3 +13,9 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
     
     serializer_class = RegisterSerializer
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # You might want to restrict this so only admins can see all users
+    permission_classes = [permissions.IsAdminUser]
